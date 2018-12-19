c292fa00b1dc9b9ebd54f8ca7dfa586141607f90
Yoshi <yoshi.cm3@gmail.com>
2018-12-19 11:50:15 -0600

    primer commit yoshi

--- app/Config/core.php
+++ app/Config/core.php
@@ -233,12 +233,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2gRH9OubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2gRH9OubWwvniR2G0FgaC9midhgs');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496745683645');
+	Configure::write('Security.cipherSeed', '768593096574535424967456836455346457');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
